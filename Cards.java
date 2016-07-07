8e69e1fa2379f95ace0d08f0e6f6b08b8a65c5b2
RajuThota <Raju.Thota@cdk.com>
2016-07-08 00:21:08 +0530

    Added Diamonds class

--- Cards.java
+++ Cards.java
@@ -1,5 +1,4 @@
  
-
 class Cards {
    
     int[] value = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
@@ -27,7 +26,7 @@ class Cards {
    
     }
  
- 
+public class Diamonds { 
     public static void main(String[] args) {
         int seq1 = 43;
         int seq2 = 47;
@@ -39,7 +38,7 @@ class Cards {
 }
 
 
-
+}
 
 
   
