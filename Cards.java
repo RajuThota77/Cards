71c97ecf484752e9c55fbac47ffa2d4a1cab9b88
RajuThota <Raju.Thota@cdk.com>
2016-07-07 13:37:07 +0530

    Constructed added

---
+++ Cards.java
@@ -0,0 +1,14 @@
+public class Cards 
+{
+    private int suites, rank;
+    private String[] suites = {"Clubs", "Spades", "Diamonds", "Hearts"};
+    private String[] rank = {"Ace", "King", "Queen", "Jack", "10",
+                                   "9", "8", "7", "6", "5", "4", "3", "2"};
+
+    public Card(int suite, int rank)
+    {
+       this.suits = suites;
+       this.rank = vrank;
+         
+    }
+}
