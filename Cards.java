0fad75f1149b2f499287e43ca6d93dc53b972f11
RajuThota <Raju.Thota@cdk.com>
2016-07-07 14:56:58 +0530

    Added methods

--- Cards.java
+++ Cards.java
@@ -1,14 +1,24 @@
-public class Cards 
+public class Cards
 {
-    private int suites, rank;
-    private String[] suites = {"Clubs", "Spades", "Diamonds", "Hearts"};
-    private String[] rank = {"Ace", "King", "Queen", "Jack", "10",
-                                   "9", "8", "7", "6", "5", "4", "3", "2"};
-
-    public Card(int suite, int rank)
-    {
-       this.suits = suites;
-       this.rank = vrank;
+  
+    	public Cards(int sequence)
+    	{
+    	   this.sequence = sequence;
          
+         }
+	public toString(int sequence) {
+	return getSuite(int sequence) + getRank(int sequence);
+	}
+
+	public int getSuite(int seq) {
+	return "CDHS"[seq/13];
+	}
+	public int getPip(int seq) {
+
+	return "A123456789JQK"[seq%13];
+	}
+    public static void main(String[] args) {
+	Crads obj = new Cards();
+	System.out.println(obj.toString());
     }
 }
