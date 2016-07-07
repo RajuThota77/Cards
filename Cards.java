080e6d12afc10337030690815d24f156ca080dcf
RajuThota <Raju.Thota@cdk.com>
2016-07-07 16:41:52 +0530

    Added Compare and Main Methods

--- Cards.java
+++ Cards.java
@@ -1,24 +1,45 @@
-public class Cards
-{
-  
-    	public Cards(int sequence)
-    	{
-    	   this.sequence = sequence;
-         
-         }
-	public toString(int sequence) {
-	return getSuite(int sequence) + getRank(int sequence);
-	}
+ 
 
-	public int getSuite(int seq) {
-	return "CDHS"[seq/13];
-	}
-	public int getPip(int seq) {
-
-	return "A123456789JQK"[seq%13];
-	}
+class Cards {
+   
+    int[] value = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    String[] suite = {"CLUBS", "DIAMONDS", "HEARTS", "SPADES"};
+    char[] pip = {'A', '2', '3', '4', '5', '6', '7', '8', '9', 't', 'J', 'Q', 'K'};
+    public int getSuite(int sequence) {
+        return sequence / 13;
+    }
+    public int getPip(int sequence) {
+        return sequence % 13;
+    }
+    public int compare(int seq1, int seq2){
+        if( getPip(seq1) > getPip(seq2) )
+            return seq1;
+        else if( getPip(seq1) < getPip(seq2))
+            return seq2;
+        else{
+            if( getSuite(seq1) > getSuite(seq2) )
+                return seq1;
+            
+            else
+                return -1;
+        }   
+   
+   
+    }
+ 
+ 
     public static void main(String[] args) {
-	Crads obj = new Cards();
-	System.out.println(obj.toString());
+        int seq1 = 43;
+        int seq2 = 47;
+        Cards c = new Cards();
+        System.out.println(c.getSuite(seq1));
+        System.out.println(c.getPip(seq2));
+        System.out.println(c.compare(seq1,seq2));
     }
 }
+
+
+
+
+
+  
