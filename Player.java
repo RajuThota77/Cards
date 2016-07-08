12ed65db301128c848cf9ca059beaee2561c11e9
Yashaswinivadapally <Yashaswini.Vadapally@cdk.com>
2016-07-08 10:07:42 +0530

    added new methods whowins,getscore into player.java

--- Player.java
+++ Player.java
@@ -1,52 +1,47 @@
 import java.util.ArrayList.*;
+
 public class Players {
-    public static void main(String[] args){
-        ArrayList<String> player1= new Arraylist<String>();
+    int gamescore=0;
     
-        player1.add("J");
-        player1.add("Q");
-        player1.add("K");
-        player1.add("A");
-        player1.add("2");
-        player1.add("3");
-        player1.add("4");
-        player1.add("5");
-        player1.add("6");
-        player1.add("7");
-        player1.add("8");
-        player1.add("9");
-        player1.add("T");
+        public Player(char suite){
+         this.suite=suite;
+        } 
+
+        ArrayList<Interger> player1= new Arraylist<Integer>();
+        pipValues = "A23456789TJQK";
+        for(int i = 0; i < pipValues.length(); i++) {
+            player1.add(new Card(pipValues.charAt(i),suite));
+      
+        Collections.shuffle(player1);
+
         
         ArrayLis <String> player2 = new ArrayList<String>();
-        player2.add("J");
-        player2.add("Q");
-        player2.add("K");
-        player2.add("A");
-        player2.add("2");
-        player2.add("3");
-        player2.add("4");
-        player2.add("5");
-        player2.add("6");
-        player2.add("7");
-        player2.add("8");
-        player2.add("9");
-        player2.add("T");
-        int diamondscore=0;
-      public static String bidrandom(ArrayList<String> list){
+          pipValues = "A23456789TJQK";
+        for(int i = 0; i < pipValues.length(); i++) {
+            player2.add(new Card(pipValues.charAt(i),'D'));        }
+        Collections.shuffle(player2);
+      public static  int bid(ArrayList<Integer> list){
                 int idx = new Random().nextInt(list.length);
-                String random = (list[idx]);
+                int random = (list[idx]);
                 return random;
-                
-    }
-    public static ArrayList<String> deletebid(int index,Arraylist<String>list){
+               
+    public static ArrayList<Integer> deletebid(int index,Arraylist<Integer>list){
         list.remove(index);
         return list;
-    }
-        
+        }
+    public static int UpdateScore(int valueofdiamond){
+       gamescore+=valueofdiamond;
+       return gamescore;
+     }
+    public static String howwins(Player player1,Player player2){
+    if(player1.gamescore>player2.gamescore)
+    return "player1";
+    else
+      return "player2";
+ }       
         
     }
     
-}
   
     
 
